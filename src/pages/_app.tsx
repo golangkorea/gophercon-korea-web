e9330fc695b99062f84ad9bc699a7d8b74591950
Sungmin Han <kenneth@pigno.se>
2023-06-21 21:40:27 +0900

    feat: add basic emotion structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Global, css } from "@emotion/react";
+import emotionReset from "emotion-reset";
+import type { AppProps } from "next/app";
+import globalCss from "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+          ${globalCss}
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
+import styled from "@emotion/styled";
+
+const Container = styled.main({
+  position: "relative",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  width: "100vw",
+  height: "100vh",
+  textAlign: "center",
+  backgroundColor: "#0af",
+});
+
+const GopherImage = styled.img({
+  width: 240,
+});
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-6 items-center justify-center bg-[#00ADD8] h-[100svh]">
+    <Container>
       <h1 className="font-mono text-white text-xl lg:text-2xl">
         Hello, <span className="font-bold">GopherCon Korea!</span>
       </h1>
-      <img className="w-40 lg:w-64" src="/gopher-plane.svg" />
-    </main>
+      <GopherImage src="/gopher-plane.svg" />
+    </Container>
   );
 }
