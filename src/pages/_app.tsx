dece4822ddda4cfb183542205313fdc4dd29e82a
hrh858 <102790398+hrh858@users.noreply.github.com>
2023-06-21 21:47:06 +0900

    Added Plus Jakarta Sans font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,22 @@ import { Global, css } from "@emotion/react";
 import emotionReset from "emotion-reset";
 import type { AppProps } from "next/app";
 import globalCss from "../styles/globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global
-        styles={css`
-          ${emotionReset}
-          ${globalCss}
-        `}
-      />
-      <Component {...pageProps} />
+      <main className={plusJakartaSans.className}>
+        <Global
+          styles={css`
+            ${emotionReset}
+            ${globalCss}
+          `}
+        />
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
