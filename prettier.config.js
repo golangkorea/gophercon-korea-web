5c0f6c581c6f240c5fe2ab34588fe5b16df7a4e9
Changhyeon Yoon <hyeon120411@gmail.com>
2023-06-27 23:51:55 +0900

    Feat : Code Guide (eslint, prettier, husky)

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 120,
+  arrowParens: "always",
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^(@/lib/(.*)$)|^(@/components/(.*)$)|^(@/styles/(.*)$)",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+};
