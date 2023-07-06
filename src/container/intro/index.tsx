95e0dd569e4edea46beeda76b7586a850fd95a57
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-07 00:06:02 +0900

    Fix : Intro Font Size

--- src/container/intro/index.tsx
+++ src/container/intro/index.tsx
@@ -7,9 +7,9 @@ const Intro = () => {
         </video>
       </div>
       <div className='margin-auto absolute bottom-0 left-0 right-0 top-0 z-20 flex flex-col items-center justify-center text-white'>
-        <h1 className='mb-7 text-7xl font-extrabold max-sm:text-5xl'>“Go in depth”</h1>
-        <h2 className='mb-24 text-5xl font-bold max-sm:mb-12 max-sm:text-3xl'>2023. 08. 05 - 06</h2>
-        <p className='text-3xl opacity-80 max-sm:text-xl'>국내 고 언어 최대 커뮤니티와 함께하세요</p>
+        <h1 className='mb-7 text-9xl font-extrabold max-sm:text-5xl'>“Go in depth”</h1>
+        <h2 className='mb-24 text-7xl font-bold max-sm:mb-12 max-sm:text-3xl'>2023. 08. 05 - 06</h2>
+        <p className='text-5xl opacity-80 max-sm:text-xl'>국내 고 언어 최대 커뮤니티와 함께하세요</p>
       </div>
     </div>
   );
