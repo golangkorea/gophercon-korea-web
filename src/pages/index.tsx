25a48f131682905a6a7554cd5e4cc235b16f0a1b
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-08 00:16:26 +0900

    Fix : Remove scroll animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,36 +4,16 @@ import Intro from "@/container/intro";
 import Location from "@/container/location";
 import Session from "@/container/session";
 import Sponsors from "@/container/sponsors";
-import React from "react";
 
 export default function Home() {
-  const [scrollY, setScrollY] = React.useState(0);
-
-  // MEMO 스크롤에 따라 하단 콘텐츠 overlay
-  React.useEffect(() => {
-    if (typeof window === "undefined") return;
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <Layout>
       <div className='relative flex min-h-screen w-screen flex-col items-center justify-center'>
         <Intro />
-        {/*<div*/}
-        {/*  className='absolute top-[100vh] mb-10 w-screen bg-background'*/}
-        {/*  style={{*/}
-        {/*    transform: `translateY(-${scrollY}px)`,*/}
-        {/*  }}*/}
-        {/*>*/}
         <About />
         <Session />
         <Sponsors />
         <Location />
-        {/*</div>*/}
       </div>
     </Layout>
   );
