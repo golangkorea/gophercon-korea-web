e044dd4e7171b11ee53a2aa56b3dfc6fc76ee7e0
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-07 18:26:16 +0900

    Feat : About Section

--- src/container/about/index.tsx
+++ src/container/about/index.tsx
@@ -1,6 +1,8 @@
 import { gsap } from "gsap";
 import React from "react";
 import { useInView } from "react-intersection-observer";
+import GoLangKoreaLogo from "/public/images/golang_korea_logo.png";
+import GopherConLogo from "/public/images/gophercon_logo.png";
 
 const About = () => {
   const [text, setText] = React.useState("국내 최초");
@@ -60,7 +62,7 @@ const About = () => {
   }, [inView]);
 
   return (
-    <section className='flex h-screen w-screen flex-col items-center justify-center gap-4 max-sm:pt-44'>
+    <section className='flex min-h-screen w-screen flex-col items-center justify-center gap-4 max-sm:py-44'>
       <div ref={ref} />
       <p className='mb-20 cursor-pointer text-5xl font-bold max-sm:text-3xl' ref={textRef}>
         {text}
@@ -70,9 +72,10 @@ const About = () => {
           <img
             src={"https://e0.pxfuel.com/wallpapers/412/505/desktop-wallpaper-golang-golang-gopher.jpg"}
             alt='gopher'
+            className='h-[240px] w-full object-cover'
           />
           <div className='flex flex-col items-center justify-center bg-gray-700 p-12'>
-            <p className='mb-8 text-2xl'>고퍼 너무 귀여워요</p>
+            <p className='mb-8 text-2xl'>블라블라</p>
             <p>
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
               Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -80,28 +83,23 @@ const About = () => {
           </div>
         </div>
         <div className='w-1/4 cursor-pointer overflow-hidden rounded-xl transition hover:scale-105 max-sm:w-full'>
-          <img
-            src={"https://e0.pxfuel.com/wallpapers/412/505/desktop-wallpaper-golang-golang-gopher.jpg"}
-            alt='gopher'
-          />
+          <img src={GopherConLogo.src} alt='gopher' className='h-[240px] w-full bg-white object-contain p-2' />
           <div className='flex flex-col items-center justify-center bg-gray-700 p-12'>
-            <p className='mb-8 text-2xl'>고퍼 너무 이상해요</p>
+            <p className='mb-8 text-2xl'>국내 최초 GopherCon</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              세상에 이건 꼭 가야해! Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
+              eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+              mus.
             </p>
           </div>
         </div>
         <div className='w-1/4 cursor-pointer overflow-hidden rounded-xl transition hover:scale-105 max-sm:w-full'>
-          <img
-            src={"https://e0.pxfuel.com/wallpapers/412/505/desktop-wallpaper-golang-golang-gopher.jpg"}
-            alt='gopher'
-          />
+          <img src={GoLangKoreaLogo.src} alt='gopher' className='h-[240px] w-full bg-white object-contain p-2' />
           <div className='flex flex-col items-center justify-center bg-gray-700 p-12'>
-            <p className='mb-8 text-2xl'>고퍼 너무 좋아요</p>
+            <p className='mb-8 text-2xl'>국내 최대 고 언어 커뮤니티</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              고랭 코리아 짱짱맨 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+              dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
             </p>
           </div>
         </div>
