4c2db953392f9712aebf6a30211c9932b513dc45
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-08 18:19:50 +0900

    Feat : Countdown animation

--- src/container/location/index.tsx
+++ src/container/location/index.tsx
@@ -1,11 +1,52 @@
-"use client";
-
 import useCountdown from "@/hooks/useCountdown";
 import dayjs from "dayjs";
+import { gsap } from "gsap";
+import React from "react";
 
 const DATE = dayjs("2023-08-05 10:00", "YYYY-MM-DD HH:mm").toDate();
 const Location = () => {
+  const daysRef = React.useRef<HTMLDivElement>(null);
+  const hoursRef = React.useRef<HTMLDivElement>(null);
+  const minutesRef = React.useRef<HTMLDivElement>(null);
+  const secondsRef = React.useRef<HTMLDivElement>(null);
   const [days, hours, minutes, seconds] = useCountdown(DATE);
+
+  const TRIGGER = {
+    day: daysRef.current,
+    hour: hoursRef.current,
+    minute: minutesRef.current,
+    second: secondsRef.current,
+  };
+
+  const tickAnimation = (trigger: keyof typeof TRIGGER) => {
+    let tl = gsap.timeline();
+    tl.to(TRIGGER[trigger], {
+      duration: 0.1,
+      y: -10,
+      ease: "power1.inOut",
+    }).to(TRIGGER[trigger], {
+      duration: 0.1,
+      y: 0,
+      ease: "power1.inOut",
+    });
+  };
+
+  React.useEffect(() => {
+    tickAnimation("day");
+  }, [days]);
+
+  React.useEffect(() => {
+    tickAnimation("hour");
+  }, [hours]);
+
+  React.useEffect(() => {
+    tickAnimation("minute");
+  }, [minutes]);
+
+  React.useEffect(() => {
+    tickAnimation("second");
+  }, [seconds]);
+
   return (
     <section className='locationGradient justify-betweenu relative flex h-[750px] w-screen items-center bg-white text-white max-lg:h-[400px]'>
       <div className='flex h-full w-full flex-col items-center justify-center'>
@@ -18,7 +59,7 @@ const Location = () => {
           </p>
         </div>
         <div className='z-10 ml-12 flex gap-4 max-lg:ml-0 max-lg:w-screen max-lg:justify-center max-lg:gap-2'>
-          <div className='countDownWrapper'>
+          <div className='countDownWrapper' ref={daysRef}>
             <div className='countDownInner'>
               <div className='countDown'>
                 <p className='text-3xl font-bold'>{days}</p>
@@ -26,7 +67,7 @@ const Location = () => {
               </div>
             </div>
           </div>
-          <div className='countDownWrapper'>
+          <div className='countDownWrapper' ref={hoursRef}>
             <div className='countDownInner'>
               <div className='countDown'>
                 <p className='text-3xl font-bold'>{hours}</p>
@@ -34,7 +75,7 @@ const Location = () => {
               </div>
             </div>
           </div>
-          <div className='countDownWrapper'>
+          <div className='countDownWrapper' ref={minutesRef}>
             <div className='countDownInner'>
               <div className='countDown'>
                 <p className='text-3xl font-bold'>{minutes}</p>
@@ -42,7 +83,7 @@ const Location = () => {
               </div>
             </div>
           </div>
-          <div className='countDownWrapper'>
+          <div className='countDownWrapper' ref={secondsRef}>
             <div className='countDownInner'>
               <div className='countDown'>
                 <p className='text-3xl font-bold'>{seconds}</p>
