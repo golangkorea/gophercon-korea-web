3eefaf2bd23345b14a28546c82fe3f608010c60b
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-08 18:21:52 +0900

    Feat : Scholarship

--- src/pages/scholarship-support/index.tsx
+++ src/pages/scholarship-support/index.tsx
@@ -1,9 +1,12 @@
+import Banner from "@/components/banner";
 import Layout from "@/components/layout";
 
 export default function ScholarshipSupportPage() {
   return (
     <Layout>
-      <div className='mt-20 max-lg:mt-16'></div>
+      <div className='mt-20 max-lg:mt-16'>
+        <Banner title={"Scholarship"} description={`장학금 제가 받고 싶네요. 얼마줘요?`} />
+      </div>
     </Layout>
   );
 }
