47dc9a8abe8505b9fec2ed9c49677e3959b1ddff
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-17 22:58:26 +0900

    Feat : COC

--- src/pages/coc/index.tsx
+++ src/pages/coc/index.tsx
@@ -11,6 +11,12 @@ export default function COCPage() {
           description={`GopherCon Korea는 모든 참가자들이 즐겁고 안전하게\n 행사에 참여할 수 있도록 최선을 다하고 있습니다.`}
         />
       </div>
+      <div className='flex h-screen items-center justify-center border border-gray-200 p-10 max-sm:p-4'>
+        <iframe
+          className='h-full w-full'
+          src='https://docs.google.com/document/d/e/2PACX-1vSPAImvAKCS31mbGTrho8Ig4hkBu2Zk-QkSxYJ4_bgmr_eHVR4HitEJGKXB62c-8Iawt7HoqpfoLSje/pub?embedded=true'
+        ></iframe>
+      </div>
     </Layout>
   );
 }
