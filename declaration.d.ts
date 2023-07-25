b7804c1ab4430f2995b07de3fbba94af467a1702
KennethanCeyer <kenneth@pigno.se>
2023-07-26 08:27:31 +0900

    fix: try to fix typing issue

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,8 +1,14 @@
 declare module "*.css";
 declare module "*.scss";
 
+interface Vanta {
+  GLOBE: (options: unknown) => void;
+}
+
 declare global {
   let VANTA: {
     GLOBE: (options: unknown) => void;
   };
 }
+
+declare var VANTA: Vanta;
