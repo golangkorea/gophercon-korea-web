89a3876166707bdc7f9dd9553b38c9ecdc867dac
Changhyeon Yoon <hyeon120411@gmail.com>
2023-07-26 00:04:55 +0900

    Feat : Sponsor Click

--- src/container/sponsors/index.tsx
+++ src/container/sponsors/index.tsx
@@ -23,12 +23,21 @@ import Google from "/public/images/sponsors/community/logo_gdg.webp";
 import Jetbrains from "/public/images/sponsors/community/logo_jetbrains.webp";
 
 // PUBLISHER
+import { SPONSORS } from "@/constants/sponsors";
+import { MouseEventHandler } from "react";
 import Decoding from "/public/images/sponsors/publisher/logo_decoding.webp";
 import GoldenRabbit from "/public/images/sponsors/publisher/logo_goldenRabbit.webp";
 import Hanbit from "/public/images/sponsors/publisher/logo_hanbitmedia.webp";
 import Jpub from "/public/images/sponsors/publisher/logo_jpub.webp";
 
 const Sponsors = () => {
+  const handleClickLogo: MouseEventHandler<HTMLImageElement> = (e) => {
+    const { id } = e.currentTarget;
+    const link = SPONSORS.find((sponsor) => sponsor.name === id)?.link;
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
   return (
     <section className='flex w-screen flex-col gap-4 bg-white px-16 py-16 max-lg:px-4'>
       <p className='mb-12 text-5xl font-extrabold'>Sponsors</p>
@@ -37,6 +46,8 @@ const Sponsors = () => {
         <div className='platinumColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
+            id='현대자동차'
             src={Hyundai}
             alt={"Hyundai Motors"}
             width={400}
@@ -50,6 +61,8 @@ const Sponsors = () => {
         <div className='goldColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
+            id='드림어스컴퍼니'
             src={Dreamus}
             alt={"Dreamus"}
             width={350}
@@ -57,6 +70,8 @@ const Sponsors = () => {
             className='h-[155px] w-4/5 cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
+            id='당근마켓'
             src={Karrot}
             alt={"Karrot"}
             width={350}
@@ -64,6 +79,8 @@ const Sponsors = () => {
             className='h-[155px] w-4/5 cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
+            id='메가존소프트'
             src={Megazon}
             alt={"Megazon"}
             width={350}
@@ -77,6 +94,7 @@ const Sponsors = () => {
         <div className='silverColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
             src={Openup}
             alt={"Openup"}
             width={300}
@@ -90,6 +108,8 @@ const Sponsors = () => {
         <div className='bronzeColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
+            id='AB180'
             src={AB180}
             alt={"AB180"}
             width={280}
@@ -97,6 +117,8 @@ const Sponsors = () => {
             className='h-[115px] w-1/6 cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
+            id='오누이(설탭)'
             src={Onuii}
             alt={"Onuii"}
             width={280}
@@ -110,6 +132,7 @@ const Sponsors = () => {
         <div className='etcColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
             src={Google}
             alt={"Google"}
             width={400}
@@ -117,6 +140,7 @@ const Sponsors = () => {
             className='h-[170px] w-4/6 cursor-pointer cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
             src={Awskrug}
             alt={"Awskrug"}
             width={250}
@@ -124,6 +148,7 @@ const Sponsors = () => {
             className='h-[115px] w-1/6 cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
             src={Jetbrains}
             alt={"Jetbrains"}
             width={250}
@@ -137,6 +162,7 @@ const Sponsors = () => {
         <div className='etcColorGradient mb-4 h-0.5 w-full' />
         <div className='flex w-full justify-center gap-8 p-4 max-lg:flex-col max-lg:items-center'>
           <Image
+            onClick={handleClickLogo}
             src={GoldenRabbit}
             alt={"GoldenRabbit"}
             width={200}
@@ -144,6 +170,7 @@ const Sponsors = () => {
             className='w-1/7 h-[115px] cursor-pointer p-2'
           />
           <Image
+            onClick={handleClickLogo}
             src={Hanbit}
             alt={"Hanbit"}
             width={200}
@@ -151,6 +178,7 @@ const Sponsors = () => {
             className='w-1/7 h-[115px] cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
             src={Decoding}
             alt={"Decoding"}
             width={200}
@@ -158,6 +186,7 @@ const Sponsors = () => {
             className='w-1/7 h-[115px] cursor-pointer bg-white object-contain p-2'
           />
           <Image
+            onClick={handleClickLogo}
             src={Jpub}
             alt={"Jpub"}
             width={200}
