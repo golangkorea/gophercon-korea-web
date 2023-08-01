f759c113f55d97336c7a03a0fd1916cda261d386
Changhyeon Yoon <hyeon120411@gmail.com>
2023-08-01 22:03:17 +0900

    Feat : Live show

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/nav";
+import useCountdown from "@/hooks/useCountdown";
 import styled from "@emotion/styled";
+import dayjs from "dayjs";
 import Script from "next/script";
 import React from "react";
 
@@ -30,11 +32,101 @@ const LayoutInner = styled.div<LayoutInnerStyledProps>(({ main }: LayoutInnerSty
   overflowY: "auto",
 }));
 
+type LiveContainerProps = {
+  isLive?: boolean;
+};
+const LiveContainer = styled.div<LiveContainerProps>(({ isLive }: LiveContainerProps) => ({
+  position: "fixed",
+  bottom: 0,
+  width: "100vw",
+  height: 80,
+  backgroundColor: "#ffffff",
+  opacity: 0.9,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  boxShadow: "0 0 10px rgba(0, 0, 0, .2)",
+  zIndex: 100,
+  "@media (max-width: 1000px)": {
+    height: 60,
+    flexDirection: "column",
+    display: isLive ? "flex" : "none",
+  },
+}));
+
+const LiveText = styled.span({
+  fontSize: 24,
+  fontWeight: 700,
+  marginRight: 20,
+  "@media (max-width: 1000px)": {
+    display: "none",
+  },
+});
+
+const CountDown = styled.span({
+  fontSize: 24,
+  fontWeight: 700,
+  textShadow: "0 3px 6px rgba(0, 163, 255, .25)",
+  background: "linear-gradient(45deg, #BD00FF, #0066FF)",
+  "-webkit-background-clip": "text",
+  "-webkit-text-fill-color": "transparent",
+  marginRight: 20,
+  "@media (max-width: 1000px)": {
+    display: "none",
+  },
+});
+
+const YoutubeButton = styled.button({
+  backgroundColor: "#FF0000",
+  padding: "8px 16px",
+  borderRadius: 16,
+  color: "#fff",
+  fontWeight: 700,
+  letterSpacing: 1,
+  "@media (max-width: 1000px)": {
+    width: "100vw",
+    borderRadius: 0,
+    padding: "16px 0",
+    letterSpacing: 2,
+  },
+});
+
+const DAY1 = dayjs("2023-08-05 10:00", "YYYY-MM-DD HH:mm");
+const DAY2 = dayjs("2023-08-06 10:00", "YYYY-MM-DD HH:mm");
+
 const Layout: React.FC<LayoutProps> = ({ children, main }) => {
+  const day: Date = React.useMemo(() => {
+    const today = dayjs();
+    if (today.isBefore(DAY1)) {
+      return DAY1.toDate();
+    } else return DAY2.toDate();
+  }, []);
+
+  const youtubeLink = React.useMemo(() => {
+    const today = dayjs();
+    if (today.isBefore(DAY1)) {
+      return "https://www.youtube.com/watch?v=WZthMW0BaNA";
+    } else return "https://www.youtube.com/watch?v=8AUVKh0qJgU";
+  }, []);
+
+  const isLive = React.useMemo(() => {
+    const today = dayjs();
+    return today.isSame(DAY1) || today.isSame(DAY2);
+  }, []);
+
+  const [days, hours, minutes, seconds] = useCountdown(day);
+
   return (
     <LayoutContainer>
       <Navbar />
-      <LayoutInner main={main}>{children}</LayoutInner>
+      <LayoutInner main={main}>
+        {children}
+        <LiveContainer isLive={isLive}>
+          <LiveText>국내에서 진행되는 Go 언어 사용자를 위한 최초의 거대 행사 GopherCon Korea 2023</LiveText>
+          <CountDown>{`${days} DAY ${hours} HOURS ${minutes} MINUTES ${seconds} SECONDS`}</CountDown>
+          {isLive && <YoutubeButton onClick={() => window.open(youtubeLink, "_blank")}>WATCH LIVE</YoutubeButton>}
+        </LiveContainer>
+      </LayoutInner>
       <Script async src='https://www.googletagmanager.com/gtag/js?id=G-T8BMXV9ZPX' />
       <Script id='google-analytics'>
         {`
