25367daf311ecb340c89f207b652130c6f0b0f11
wooseok <hosmimam@naver.com>
2024-08-29 00:30:41 +0900

    feat : renew emotion registry file

--- packages/gophercon-common/src/style/registry.tsx
+++ packages/gophercon-common/src/style/registry.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 import { useServerInsertedHTML } from "next/navigation";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
-export const EmotionStyleRegistry = ({ children }: { children: React.ReactNode }) => {
+export default function EmotionStyleRegistry({ children }: { children: ReactNode }) {
   const [{ cache, flush }] = useState(() => {
     const cache = createCache({ key: "my" });
     cache.compat = true;
-
     const prevInsert = cache.insert;
     let inserted: string[] = [];
-
     cache.insert = (...args) => {
       const serialized = args[1];
       if (cache.inserted[serialized.name] === undefined) {
@@ -20,25 +18,21 @@ export const EmotionStyleRegistry = ({ children }: { children: React.ReactNode }
       }
       return prevInsert(...args);
     };
-
     const flush = () => {
       const prevInserted = inserted;
       inserted = [];
       return prevInserted;
     };
-
     return { cache, flush };
   });
 
   useServerInsertedHTML(() => {
     const names = flush();
     if (names.length === 0) return null;
-
     let styles = "";
     for (const name of names) {
       styles += cache.inserted[name];
     }
-
     return (
       <style
         data-emotion={`${cache.key} ${names.join(" ")}`}
@@ -50,4 +44,4 @@ export const EmotionStyleRegistry = ({ children }: { children: React.ReactNode }
   });
 
   return <CacheProvider value={cache}>{children}</CacheProvider>;
-};
+}
