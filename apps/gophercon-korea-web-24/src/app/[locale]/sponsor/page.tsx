c972fb9702fca6bc062ed538a21ef08911437fad
KennethanCeyer <kenneth@pigno.se>
2024-10-11 03:23:59 +0900

    fix: design of sponsors

--- apps/gophercon-korea-web-24/src/app/[locale]/sponsor/page.tsx
+++ apps/gophercon-korea-web-24/src/app/[locale]/sponsor/page.tsx
@@ -104,14 +104,14 @@ const SponsorModalWrapper = styled.div({
 const SponsorModalOuter = styled.div({
   backgroundColor: "#ffffff",
   padding: 80,
-  borderRadius: 80,
+  borderRadius: 40,
   width: "70vw",
   maxWidth: "1000px",
   margin: "0 auto",
   position: "relative",
   "@media (max-width: 1100px)": {
-    padding: "40px",
-    borderRadius: "20px",
+    padding: 40,
+    borderRadius: 20,
   },
 });
 
@@ -120,11 +120,11 @@ const SponsorModalInner = styled.div({});
 const ModalCloseBtn = styled(Image)({
   cursor: "pointer",
   position: "absolute",
-  right: "30px",
-  top: "30px",
-  "@media (max-width: 1100px)": {
-    right: "10px",
-    top: "10px",
+  right: -40,
+  top: -40,
+  "@media (max-width: 960px)": {
+    right: 10,
+    top: 10,
   },
 });
 
@@ -143,16 +143,16 @@ const SponsorModalImage = styled.img({
   objectFit: "contain",
   width: "auto",
   height: "100%",
-  "@media (max-width: 500px)": {
-    height: "auto",
+  "@media (max-width: 960px)": {
     width: "100%",
+    maxHeight: 140,
   },
 });
 
 const SponsorModalLabel: FC<ModalLableProps> = ({ bgColor, label }) => {
   return (
     <SponsorModalLabelWrapper bgColor={bgColor}>
-      <Text weight={700} size={"1.5rem"}>
+      <Text weight={700} size={"1.25rem"}>
         {label}
       </Text>
     </SponsorModalLabelWrapper>
@@ -170,20 +170,29 @@ const SponsorModalImageWrapper = styled.div({
 
 const SponsorModalCTA = styled.div({
   width: "100%",
-  backgroundColor: "#6630D9",
+  backgroundColor: "#6630d9",
   display: "flex",
   justifyContent: "center",
   borderRadius: "20px",
+  "&:hover": {
+    backgroundColor: "#7234e1",
+  },
 });
 
-const SponsorModalCTAInner = styled.div({
+const SponsorModalCTAInner = styled(Link)({
   padding: "20px",
+  fontSize: "1.5em",
+  fontWeight: 600,
+  textAlign: "center",
+  color: "#ffffff",
+  cursor: "pointer",
 });
 
 const SponsorDescWrapper = styled.div({
   height: "200px",
   overflowY: "scroll",
   width: "100%",
+  lineHeight: 1.6,
 });
 
 const SponsorLabelFlex = styled.div({
@@ -236,21 +245,22 @@ const SponsorModal: FC<SponsorProps> = ({ imgSrc, grade, desc, link, name }) => 
                 </Flex>
               </SponsorModalImageWrapper>
               <SponsorDescWrapper>
-                <Text size={"1.5rem"} weight={700}>
+                <Text size={"1.25rem"} weight={700}>
                   {desc[locale]}
                 </Text>
               </SponsorDescWrapper>
               <SponsorModalCTA id='cta'>
-                <SponsorModalCTAInner>
-                  <Link href={link} target='_blank' style={{ textDecoration: "none", width: "100%" }} id='cta'>
-                    <Text align='center' size={"1.5rem"} color='#ffffff' id='cta'>
-                      {dict.sponsor.cta}
-                    </Text>
-                  </Link>
+                <SponsorModalCTAInner
+                  href={link}
+                  target='_blank'
+                  style={{ textDecoration: "none", width: "100%" }}
+                  id='cta'
+                >
+                  {dict.sponsor.cta}
                 </SponsorModalCTAInner>
               </SponsorModalCTA>
             </SponsorLabelFlex>
-            <ModalCloseBtn onClick={closeModal} src={"/modalClose.svg"} alt='modalClose' width={30} height={30} />
+            <ModalCloseBtn onClick={closeModal} src={"/modalClose.svg"} alt='modalClose' width={40} height={40} />
           </SponsorModalInner>
         </SponsorModalOuter>
       </Flex>
